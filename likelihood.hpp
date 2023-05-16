95d49ce961dc1ccc90e0be16d9732b69ce455d1c
fgregoretti <72740332+fgregoretti@users.noreply.github.com>
2023-05-16 13:26:49 +0200

    Add files via upload

--- likelihood.hpp
+++ likelihood.hpp
@@ -10,11 +10,12 @@
 #include <assert.h>
 #include <algorithm>
 #include <cmath>
+#include <cstring>
 #include <ctime>
 #include "common.h"
-#include "cross.hpp"
 #include "util.hpp"
 #include "constants.h"
+#include "beliefs.hpp"
 #ifdef _OPENMP
 #include "omp.h"
 #endif
@@ -75,9 +76,22 @@ namespace detail
       return t[i];
     }
 
+    likelihood& operator=(const likelihood<T,seq<Iseq...>>& rhs)
+    {
+      for (std::size_t i = 0; i < mult(s); ++i)
+        t[i] = rhs.t[i];
+
+      return *this;
+    }
+
     void setValue(T value, decltype(Iseq)... i)
     {
       t[index({{ i... }})] = value;
+    }
+
+    void setValue(T value, std::size_t i)
+    {
+      t[i] = value;
     }
 
     void Zeros()
@@ -157,6 +171,16 @@ namespace detail
 #endif
       for (std::size_t i = 0; i < mult(s); ++i)
         t[i] += b.t[i];
+    }
+
+    /* sum of this object and likelihood object given as a parameter */
+    void sum(const likelihood<T,seq<Iseq...>>& b, T e)
+    {
+#ifdef _OPENMP
+      #pragma omp parallel for
+#endif
+      for (std::size_t i = 0; i < mult(s); ++i)
+        t[i] += b.t[i] * e;
     }
 
     /* return order (rank) */
@@ -350,7 +374,7 @@ namespace detail
 
       _q=new T[s[0]];
 
-      T *x = cross(xt, s);
+      T *x = cross(xt);
 
       std::size_t offset = mult(s)/s[0];
 
@@ -404,7 +428,7 @@ namespace detail
 
       _q=new T[s[0]];
 
-      T *x = cross(xt, s);
+      T *x = cross(xt);
 
       std::size_t offset = mult(s)/s[0];
 
@@ -451,7 +475,7 @@ namespace detail
     {
       T H = 0;
 
-      T *x = cross(xt, s);
+      T *x = cross(xt);
 
       std::size_t offset = mult(s)/s[0];
 
@@ -496,6 +520,167 @@ namespace detail
 
         p.at(k) = t[ind];
       }
+    }
+
+    /* Multidimensional cross (outer) product */
+    T *cross(T **arr)
+    {
+      /* number of arrays */
+      std::size_t n = s.size()-1;
+
+      std::size_t m = s[1];
+      for (std::size_t i = 2; i <= n; i++)
+        m *= s[i];
+      T *y = new T[m];
+      memset(y, 0, m*sizeof(T));
+
+      if (n == 1)
+      {
+        for (std::size_t i = 0; i < m; i++)
+          y[i] = arr[0][i]; 
+        return y;
+      }
+
+      std::size_t m1 = s[1];
+ 
+#ifdef _OPENMP
+  #pragma omp parallel for
+#endif
+      for (std::size_t j = 0; j < m1; j++)
+      {
+        std::size_t m2 = m/m1;
+
+        /* to keep track of next element in each of
+           the n-1 arrays */
+        std::size_t *indices = new std::size_t[n];
+        indices[0] = j;
+        for (std::size_t i = 1; i < n; i++)
+          indices[i] = 0;
+
+        for (std::size_t count = 0; count < m2; count++) {
+          /* compute current product */
+          T product = arr[0][indices[0]];
+
+          for (std::size_t i = 1; i < n; i++)
+            product *= arr[i][indices[i]];
+
+          y[j*m2+count] = product;
+
+          /* find the rightmost array that has more
+             elements left after the current element
+             in that array */
+          int next = n - 1;
+          while (next >= 1 &&
+                (indices[next] + 1 >= s[next+1]))
+            next--;
+ 
+          if (next >=1) {
+            /* if found move to next element in that
+               array */
+            indices[next]++;
+ 
+            /* for all arrays to the right of this
+               array current index again points to
+               first element */
+            for (std::size_t i = next + 1; i < n; i++)
+              indices[i] = 0;
+          }
+        }
+
+        delete [] indices;
+      }
+
+      return y;
+    }
+
+    /* Multidimensional cross (outer) product */
+    void cross(unsigned int d, unsigned int tt, std::vector<Beliefs<T>*> &_X)
+    {
+      /* number of arrays */
+      std::size_t n = s.size()-1;
+
+      std::size_t m = s[1];
+      for (std::size_t i = 2; i <= n; i++)
+      m *= s[i];
+
+      std::size_t _offset = m * d;
+      for (std::size_t i = 0; i < m * s[0]; ++i)
+        t[i] = 0.0;
+
+      if (n == 1)
+      {
+        for (std::size_t i = 0; i < m; i++)
+          t[_offset+i] = _X[0]->getValue(i,tt); 
+        return;
+      }
+
+      std::size_t m1 = s[1];
+ 
+#ifdef _OPENMP
+  #pragma omp parallel for
+#endif
+      for (std::size_t j = 0; j < m1; j++)
+      {
+        std::size_t m2 = m/m1;
+
+        /* to keep track of next element in each of
+           the n-1 arrays */
+        std::size_t *indices = new std::size_t[n];
+        indices[0] = j;
+        for (std::size_t i = 1; i < n; i++)
+          indices[i] = 0;
+
+        for (std::size_t count = 0; count < m2; count++) {
+          /* compute current product */
+          T product = _X[0]->getValue(indices[0],tt);
+
+          for (std::size_t i = 1; i < n; i++)
+            product *= _X[i]->getValue(indices[i],tt);
+
+          //std::cout << "index = " << _offset+j*m2+count << " product = " << product << std::endl;
+          t[_offset+j*m2+count] = product;
+
+          /* find the rightmost array that has more
+             elements left after the current element
+             in that array */
+          int next = n - 1;
+          while (next >= 1 &&
+                (indices[next] + 1 >= s[next+1]))
+            next--;
+ 
+          if (next >=1) {
+            /* if found move to next element in that
+               array */
+            indices[next]++;
+ 
+            /* for all arrays to the right of this
+               array current index again points to
+               first element */
+            for (std::size_t i = next + 1; i < n; i++)
+              indices[i] = 0;
+          }
+        }
+
+        delete [] indices;
+      }
+
+      return;
+    }
+
+    /* fills up the array elements applying a mask to the first likelihood object
+       array; the mask is obtained by comparing each element of the second likelihood
+       object array with zero */
+    void multiplies(const likelihood<T,seq<Iseq...>>& a,
+                    const likelihood<T,seq<Iseq...>>& b)
+    {
+#ifdef _OPENMP
+      #pragma omp parallel for
+#endif
+      for (std::size_t i = 0; i < mult(s); ++i)
+        if (b.t[i] > 0)
+          t[i] = a.t[i];
+	else
+	  t[i] = 0.0;
     }
  
   private:
